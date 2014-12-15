44f2cd4d9f5919fcaea8716f8ca70dcb9bb09040
Spiker <spiker@Mac-mini-Spiker.local>
2014-12-15 15:49:41 +0300

    index

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,13 +1,13 @@
 <?php
 
-defined('DATA_PATH') or define('DATA_PATH',__DIR__.'/../data');
+defined('DATA_PATH') or define('DATA_PATH',__DIR__.'/../../../data');
 defined('CONF_PATH') or define('CONF_PATH',__DIR__.'/../../../conf');
 defined('BASE_PATH') or define('BASE_PATH',dirname(__FILE__).'/..');
 
 defined('RNTM_PATH') or define('RNTM_PATH',DATA_PATH.'/runtime');
 defined('DB_PATH') or define('DB_PATH',CONF_PATH.'/database');
 
-defined('ASSETS_PATH') or define('ASSETS_PATH',dirname(__FILE__).'/assets');
+defined('ASSETS_PATH') or define('ASSETS_PATH',DATA_PATH.'/assets');
 defined('ASSETS_URL') or define('ASSETS_URL','/assets');
 
 
