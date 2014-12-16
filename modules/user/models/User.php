6833d73b6a32ee50630513ff6dc6dd5a69c077ed
Spiker <spiker@Mac-mini-Spiker.local>
2014-12-16 17:41:11 +0300

    soc

--- modules/user/models/User.php
+++ modules/user/models/User.php
@@ -102,11 +102,33 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
             return new self(Yii::$app->getSession()->get('user-'.$id));
         }
         else {
-            return static::findOne(['id' => $id]);
-            //return isset(self::$users[$id]) ? new self(self::$users[$id]) : null;
+//            return static::findOne(['id' => $id]);
+            return isset(self::$users[$id]) ? new self(self::$users[$id]) : null;
         }
 
     }
+    /**
+     * @param \nodge\eauth\ServiceBase $service
+     * @return User
+     * @throws ErrorException
+     */
+    public static function findByEAuth($service) {
+        if (!$service->getIsAuthenticated()) {
+            throw new ErrorException('EAuth user should be authenticated before creating identity.');
+        }
+
+        $id = $service->getServiceName().'-'.$service->getId();
+        $attributes = array(
+            'id' => $id,
+            'username' => $service->getAttribute('name'),
+            'authKey' => md5($id),
+            'profile' => $service->getAttributes(),
+        );
+        $attributes['profile']['service'] = $service->getServiceName();
+        Yii::$app->getSession()->set('user-'.$id, $attributes);
+        return new self($attributes);
+    }
+
 
      /**
      * @inheritdoc
