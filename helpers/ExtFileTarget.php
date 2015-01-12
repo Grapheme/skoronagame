1ab92bc85bc59297e117f4df27b53698c84212d7
Spiker <spiker@Mac-mini-Spiker.local>
2015-01-12 11:45:05 +0300

    ext error log

---
+++ helpers/ExtFileTarget.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\helpers;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\log;
+use vova07\console\ConsoleRunner;
+
+class  ExtFileTarget extends log\FileTarget
+{
+    public function init()
+    {
+        parent::init();
+
+        if(Yii::$app->errorHandler) {
+            $cmd = PHP_BINDIR . '/php '.Yii::$app->basePath.'/yii socket/init';
+            $usr = shell_exec($cmd);
+
+            Yii::info('fatal','sserver');}
+    }
+}
