f295a30fd4dd10dfe64eb0bf601c337614ad3dfd
Владимир <vkharseev@gmail.com>
2015-04-22 13:10:54 +0300

    skoronagame

--- app/config/digitalocean/database.php
+++ app/config/digitalocean/database.php
@@ -13,7 +13,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'skorona',
+			'database'  => 'skoronagame',
 			'username'  => 'root',
 			'password'  => 'perl03',
 			'charset'   => 'utf8',
