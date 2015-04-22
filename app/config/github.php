f080ed124f42fe01b7df5c9f0c4c6afa878d31e7
Владимир <vkharseev@gmail.com>
2015-04-22 13:20:51 +0300

    git config

--- app/config/github.php
+++ app/config/github.php
@@ -8,7 +8,7 @@ return array(
     'remote'            => 'origin',
     'branch'            => 'master',
     'repository_name'   => 'skoronagame',
-    'repository_id'     => 24047268,
+    'repository_id'     => 34341384,
     'user_group'        => 'www-data',
     'user_name'         => 'www-data',
     'set_log'           => TRUE,
