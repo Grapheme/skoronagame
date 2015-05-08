cc0ed09b0d6149f2aceb2a7f90dadbc2cea29b82
etomarat <etomarat@gmail.com>
2015-05-08 14:08:24 +0300

    верстка

---
+++ public/theme/desktop/app/scripts/main.js
@@ -0,0 +1,98 @@
+/* jshint devel:true */
+console.log('\'Allo \'Allo!');
+
+var bg_width = $('#map').width();
+
+function scale() {
+  var doc_width = $(window).width();
+  $('#map').transition({ scale: doc_width/bg_width });
+}
+
+scale();
+
+$(window).resize(function (){
+  scale();
+});
+
+
+$('.areas .countur svg path').hover(function(){
+  $(this).closest('.area').toggleClass('active');
+});
+
+var _history = [];
+var _history = ['menu'];
+
+function openFrame(href) {
+  if (href=='close') {
+    _history.pop();
+  } else {
+    _history.push(href);
+  }
+  var last_item = _history[_history.length-1]
+  $('.popup-wrapper .popup-holder .popup').removeClass('active');
+  $('.popup-wrapper .popup-holder .popup#'+last_item).addClass('active');
+}
+
+$('body').on('click', 'a',function(e){
+  var href = $(this).attr('href').split('#');
+  //console.log(href[1]);
+  if (href[1]) {
+    openFrame(href[1]);
+    e.preventDefault();
+  }
+});
+
+$('body').on('click', '.numpad a', function(e){
+  var $input = $(this).closest('.numpad').prev('form').find('input');
+  $input.focus();
+  if ($(this).hasClass('clear')) {
+    $input.val('');
+  } else if ($(this).hasClass('del')) {
+    $input.val($input.val().slice(0, -1));
+  } else {
+    $input.val($input.val()+$(this).text());
+  }
+  e.preventDefault();
+});
+
+$('body').on('click', '.popup.with-tabs .tabs-btn a', function(e){
+  e.preventDefault();
+  var _c = $('.popup.with-tabs .tabs-btn a').index($(this));
+  if (!$(this).is('.active')) {
+    $('.popup.with-tabs .tabs-btn a').removeClass('active');
+    $(this).addClass('active');
+    $('.popup.with-tabs .tabs .tab').removeClass('active');
+    $('.popup.with-tabs .tabs .tab').eq(_c).addClass('active');
+  }
+});
+
+$('#help .q').click(function(){
+  $(this).toggleClass('active');
+  $(this).next('.a').slideToggle();
+});
+
+if (_skoronagame_.open_frame) {
+  openFrame(_skoronagame_.open_frame);
+}
+
+$('form').submit(function(e){
+  e.preventDefault();
+  var _href=$(this).attr('action');
+  var _method=$(this).attr('method');
+  $.ajax({
+    type: _method,
+    url: _href,
+    data: $(this).serialize(),
+    success: function (response) {
+      console.log(response)
+      if (response.status == 'ok') {
+        openFrame(response.open_frame);
+      }
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.log(xhr)
+      console.log(textStatus)
+    }
+  });
+
+});
