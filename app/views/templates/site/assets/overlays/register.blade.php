694a0480ff1d71edea750327faf26605a7c7fcd2
etomarat <etomarat@gmail.com>
2015-05-12 13:20:20 +0300

    поле ввода имени в форме регистрации

--- app/views/templates/site/assets/overlays/register.blade.php
+++ app/views/templates/site/assets/overlays/register.blade.php
@@ -2,6 +2,17 @@
     <a href="#close" class="close"></a>
     <div class="title">Регистрация</div>
     {{ Form::open(array('url'=>URL::route('quick-register'),'data-result'=>'authorisation-send')) }}
+        <input name="text" type="text" placeholder="Введите имя">
+        <div class="i">
+          <div class="text-wrapper">
+            <div class="text">
+              “Имя” должно быть уникальным,
+              может содержать кириллические
+              и латинские буквы, цифры, знак пробела.
+              Длина не более 12 символов.
+            </div>
+          </div>
+        </div>
         {{ Form::email('email',NULL,array('placeholder'=>'Ваш e-mail')) }}
         {{ Form::button('Отправить',array('type'=>'submit')) }}
     {{ Form::close() }}
