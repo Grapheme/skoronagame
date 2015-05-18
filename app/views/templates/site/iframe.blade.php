8b1cbc6930b1d31bad651c5d7a8e5b859ced3dab
Владимир <vkharseev@gmail.com>
2015-05-18 19:03:29 +0300

    iframe

--- app/views/templates/site/iframe.blade.php
+++ app/views/templates/site/iframe.blade.php
@@ -4,14 +4,16 @@
  * AVAILABLE_ONLY_IN_ADVANCED_MODE
  */
 ?>
-@extends(Helper::layout())
-@section('style')
-@stop
-@section('content')
-    <iframe src="{{ pageurl('mainpage') }}" width="1024" height="768" align="left">
-        Ваш браузер не поддерживает плавающие фреймы!
-    </iframe>
-@stop
-
-@section('scripts')
-@stop
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+</head>
+<body>
+<iframe src="{{ pageurl('mainpage') }}" width="1024" height="768" align="left">
+    Ваш браузер не поддерживает плавающие фреймы!
+</iframe>
+</body>
+</html>
