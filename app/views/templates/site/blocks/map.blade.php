f51a51161895a755a0d35d9b30651f8de0ed6557
etomarat <etomarat@gmail.com>
2015-05-19 20:14:24 +0300

    merge

--- app/views/templates/site/blocks/map.blade.php
+++ app/views/templates/site/blocks/map.blade.php
@@ -1,17 +1,19 @@
 <div class="temp-map"></div>
 <div id="map">
     <div class="areas">
-        <div id="area-12" class="area">
+        @for ($i = 1; $i < 15; $i++)
+        <div id="area-{{ $i }}" class="area">
             <div class="undeground"></div>
             <div class="ground"></div>
             <div class="countur">
-                {{ File::get(public_path(Config::get('site.theme_path').'/images/12_contyr.svg')) }}
+                {{ File::get(public_path(Config::get('site.theme_path').'/images/'.$i.'_contyr.svg')) }}
             </div>
             <div class="glow">
-                {{ File::get(public_path(Config::get('site.theme_path').'/images/12_glow.svg')) }}
+                {{ File::get(public_path(Config::get('site.theme_path').'/images/'.$i.'_glow.svg')) }}
             </div>
             <div class="objects"></div>
         </div>
+        @endfor
     </div>
     <img src="{{ asset(Config::get('site.theme_path').'/images/bg.jpg') }}" class="bg">
 </div>
