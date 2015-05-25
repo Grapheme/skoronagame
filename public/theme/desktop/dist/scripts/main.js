966abdbdf6ec04f1f0ade6434d8dff66f9457e4e
etomarat <etomarat@gmail.com>
2015-05-25 19:52:02 +0300

    fix

--- public/theme/desktop/dist/scripts/main.js
+++ public/theme/desktop/dist/scripts/main.js
@@ -3,7 +3,7 @@
  */
 
 var GAME = GAME || {};
-GAME.game_id = 1;                                       // id игры
+GAME.game_id = 0;                                       // id игры
 GAME.user = {};                                         // пользователь
 GAME.enemies = [];                                         // враги
 GAME.status = 0;                                        // статус игры
