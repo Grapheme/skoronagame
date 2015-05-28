f5efc5f5ab50f4f57c9847202983ea691072a4ba
Владимир <vkharseev@gmail.com>
2015-05-28 19:22:16 +0300

    fix 2.2.1

--- app/modules/application/game.controller.php
+++ app/modules/application/game.controller.php
@@ -901,7 +901,7 @@ class GameController extends BaseController {
         $status = FALSE;
         if ($this->validGameStatus($this->game_statuses[2])):
             if ($user->status == 0 && $user->available_steps > 0):
-                if ($user->make_steps > $user->available_steps):
+                if ($user->available_steps > $user->make_steps):
                     $user->make_steps = $user->make_steps + 1;
                     $user->save();
                     $user->touch();
