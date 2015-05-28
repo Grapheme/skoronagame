a6148d80414b38c6d873c5110db5a7e17fe5b538
Владимир <vkharseev@gmail.com>
2015-05-28 18:54:43 +0300

    fix

--- app/modules/application/game.controller.php
+++ app/modules/application/game.controller.php
@@ -362,7 +362,7 @@ class GameController extends BaseController {
                         endif;
                         if ($this->game_winners === 'standoff'):
                             $this->resetQuestions();
-                        elseif(!empty($this->game_winners) && is_array($this->game_winners)):
+                        elseif(!empty($this->game_winners) && is_array($this->game_winners) && count($this->game_winners) == $number_participants):
 //                            GameUser::where('game_id', $this->game->id)->update(array('status' => 0, 'make_steps' => 0, 'updated_at' => date('Y-m-d H:i:s')));
                             foreach ($this->game_winners as $user_id => $place):
                                 GameUserQuestions::where('game_id', $this->game->id)->where('user_id', $user_id)->where('status', 1)->where('place', 0)
